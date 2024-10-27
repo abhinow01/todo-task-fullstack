66984008ef7a8bb8f26358adc06195d81234d560
abhinav khanna <kabhinav99@gmail.com>
2024-10-27 13:05:15 +0530

    added home page

--- frontend/vite-project/src/HomePage.jsx
+++ frontend/vite-project/src/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="max-w-md w-full bg-white rounded-lg shadow-lg overflow-hidden">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 relative">
+      <div className="max-w-md w-full bg-white rounded-lg shadow-lg overflow-hidden absolute bottom-0">
         <div className="bg-blue-500 h-80 relative flex flex-col items-center justify-center">
           <div className="absolute top-6 right-6 bg-blue-300 rounded-full w-12 h-12"></div>
           <div className="absolute top-1/4 left-6 w-12 h-12 text-white opacity-25">
@@ -19,7 +21,10 @@ const HomePage = () => {
           <p className="text-gray-600 text-center mb-6">
             The best way to manage what you have to do, don’t forget your plans
           </p>
-          <button className="w-full bg-blue-500 text-white py-3 rounded-lg font-semibold hover:bg-blue-600 transition duration-200" onClick={()=>{navigate('/todo')}}>
+          <button
+            className="w-full bg-blue-500 text-white py-3 rounded-lg font-semibold hover:bg-blue-600 transition duration-200"
+            onClick={() => navigate('/todo')}
+          >
             Get Started
           </button>
         </div>
